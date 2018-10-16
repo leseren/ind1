3de846802b1f4c5c29d8d8669f69dcb237d36891
Глеб Чер <ogrttt@gmail.com>
2018-10-17 01:29:36 +0300

    Добавьте файлы проекта.

---
+++ ind1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "product.h"
+using namespace std;
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	stock a = all_sozd();
+	
+
+	system("pause");
+	return 0;
+}
---
+++ ind1/product.h
@@ -0,0 +1,105 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class stock;
+class product
+{
+
+private:
+	string name_product;
+	size_t quantity;
+	size_t price;
+public:
+	product(string name_product="", size_t quantity=0, size_t price=0) : name_product(name_product), quantity(quantity), price(price) {};
+	string get_name_product()const { return name_product; }
+	size_t get_quantity()const { return quantity; }
+	size_t get_price()const { return price; }
+	void set_name_product(string a) { name_product=a; }
+	void set_quantity(size_t a) { quantity=a; }
+	void set_price(size_t a) { price=a; }
+	void print_all() { cout << name_product << " " << quantity << price << endl; }
+	void set_all(string a, size_t b, size_t c) { name_product = a; quantity = b; price=c;}
+	
+};
+
+class stock
+{
+private:
+	string name;//им€ склада
+	size_t size;//сколько там хронитьс€ добра
+	product *product_in_stok;//само добро 
+public:
+	stock(string name="", size_t size=1) :name(name), size(size)
+	{
+		product_in_stok = new product[size];
+		for (int i = 0; i < size; ++i) product_in_stok[i];
+	}
+
+	stock(string name, size_t size, product *product_in_stok1) :name(name), size(size) 
+	{
+		
+		product_in_stok = new product[size];
+		for (int i = 0; i < size; ++i)
+			product_in_stok[i] = product_in_stok1[i];
+	}
+
+	stock(const stock & d) :name(name), size(size)
+	{
+		product_in_stok = new product[size];
+		for (int i = 0; i < size; ++i)
+			product_in_stok[i] = d.product_in_stok[i];
+	}
+
+	~stock()
+	{
+		size=0;
+		delete[] product_in_stok;
+	}
+	string get_name() { return name; }
+	size_t get_size() { return size; }
+	product operator[](int index) const { return product_in_stok[index]; }
+	product &operator[](int index)  { return product_in_stok[index]; }
+	string get_max_price()
+	{
+		int max = product_in_stok[0].get_price(), number=0;
+		for (int i = 1; i < size; ++i)
+		{
+			if (product_in_stok[i].get_price() > max)
+			{
+				max = product_in_stok[0].get_price();
+				number = i;
+			}
+		}
+		return product_in_stok[number].get_name_product();
+	}
+	bool chek(string name)
+	{
+		for (int i = 0; i < size; ++i)
+			if (product_in_stok[i].get_name_product() == name) return true;
+		return false;
+	}
+	
+
+	friend stock all_sozd();
+};
+
+stock all_sozd()
+{
+	string name;
+	size_t size, size1;
+	cout << "¬видите им€ склада ";
+	cin >> name;
+	cout << " ввидите количество товара ";
+	cin >> size;
+	cout << endl;
+	stock all(name, size);
+	for (int i = 0; i < size; ++i)
+	{
+		cout << "¬видите им€ товара, количество, чену ";
+		cin >> name >> size >> size1;
+		all.product_in_stok[i].set_all(name, size, size1);
+	}
+	return all;
+}
+
